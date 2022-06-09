ae504d7732872dc58433580cdbad2a09c561b996
knowsoeun <xxxxxxx94@naver.com>
2022-06-09 14:46:12 +0900

    소은 test

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return <div className="App">안녕하세요 저는 태평양 어깨를 가진 프로그래머 입니다.</div>;
+    return <div className="App">안녕하세요 저는 태평양 어깨를 가진 소은 프로그래머 입니다.</div>;
 }
 
 export default App;
